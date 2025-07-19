6eff0eb44cfef3bc2fffd2893a9f57627308cedd
jsousou77 <jesousou2414@gmail.com>
2025-07-19 16:10:07 -0400

    fix confirm, add concurrent video/audio download

--- fmdl.sh
+++ fmdl.sh
@@ -26,7 +26,7 @@ elif [[ $2 != *".mp4" && $2 != *".mkv" ]]; then
 fi
 
 if [[ -e "$2" ]]; then
-  println "Overwrite \"$2\"? " $BOLD -n
+  println "Overwrite \"$2\"? (y/N): " $BOLD -n
   read confirm
   if [[ "$confirm" == [yY] ]]; then
     rm -f "$2"
@@ -41,8 +41,9 @@ video_download_url=$(yt-dlp -gf bv "$1")
 if [[ $video_download_url != *"chunklist_vo"* ]]; then
   println "This video is not compatible with this script!" $ERROR
   println "Try with yt-dlp? (y/N): " $BOLD -n
+  unset confirm
   read confirm
-  if [[ $confirm == [yY] ]]; then
+  if [[ "$confirm" == [yY] ]]; then
     yt-dlp -o "$2" "$1"
   else
     println "Exiting" $INFO
@@ -52,12 +53,21 @@ fi
 
 rand=$RANDOM
 
-println "Downloading video..." $INFO
-yt-dlp -o fmdl.$rand.video.mp4 "$video_download_url"
+println "Downloading video in background..." $INFO
+tmp_log=$(mktemp)
+yt-dlp --color always -o fmdl.$rand.video.mp4 "$video_download_url" >"$tmp_log" 2>&1 &
+ytdl_pid=$!
 
 println "Downloading audio..." $INFO
 audio_download_url=$(echo $video_download_url | sed "s/chunklist_vo_/chunklist_audio_/")
 yt-dlp -o fmdl.$rand.audio.m4a "$audio_download_url"
+
+println "Waiting for video download..." $INFO
+tail -f "$tmp_log" &
+tail_pid=$!
+wait $ytdl_pid
+kill $tail_pid
+rm "$tmp_log"
 
 println "Combining video and audio..." $INFO
 ffmpeg -i fmdl.$rand.video.mp4 -i fmdl.$rand.audio.m4a -c copy "$2"
